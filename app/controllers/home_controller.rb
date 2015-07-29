4d53cc5409ba3e419a8f490f8d8eee69063de146
fushang318 <fushang318@gmail.com>
2015-07-29 17:27:49 +0800

    rails_apps_composer: initial commit

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  before_filter :authenticate_user!#, only: [:dashboard]
-  def index
-  end
-end
--- config/application.rb
+++ config/application.rb
@@ -17,18 +17,6 @@ Bundler.require(*Rails.groups)
 
 module TryRethinkdb
   class Application < Rails::Application
-
-    config.generators do |g|
-      g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: false,
-        request_specs: false
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -39,6 +27,6 @@ module TryRethinkdb
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :'zh-CN'
+    # config.i18n.default_locale = :de
   end
 end
