208523e86d3900adbf6db047128334cddb6df8e1
fushang318 <fushang318@gmail.com>
2015-07-29 17:29:00 +0800

    rails_apps_composer: set up database

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,21 +1,24 @@
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner[:mongoid].clean_with(:truncation)
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
+    #DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner[:mongoid].strategy = :truncation
   end
 
   config.before(:each, :js => true) do
-    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner[:mongoid].strategy = :truncation
+    #DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
-    DatabaseCleaner.start
+    DatabaseCleaner[:mongoid].start
   end
 
   config.append_after(:each) do
-    DatabaseCleaner.clean
+    DatabaseCleaner[:mongoid].clean
   end
 end
+
